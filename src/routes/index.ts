d783195dba58e4e616319c3998e26d71882226ec
Edgar Parucho <e.parucho@outlook.com>
2025-01-17 10:36:21 -0400

    Configures basic routes handling (NB-df4f).

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,15 +5,30 @@ import Service from '../services';
 const router = express.Router();
 const service = new Service();
 
-router.get('/api',  function(req: Request, res: Response) {
-  res.send('Hello world!');
-});
-
 router.post('/api', function(req: Request, res: Response) {
   service.create(req.body as Partial<Note>)
     .then((note: Note) => res.json(note))
     .catch((err: Error) => res.status(400));
 })
+
+router.get('/api',  function(req: Request, res: Response) {
+  const author = 'auth0|1234567890';
+  service.read(author)
+    .then((notes: Note[]) => res.json(notes))
+    .catch((err: Error) => res.status(400));
+});
+
+router.patch('/api/:id', function(req: Request, res: Response) {
+  service.update(req.params.id, req.body as Partial<Note>)
+    .then(() => res.sendStatus(200))
+    .catch((err: Error) => res.status(400));
+});
+
+router.delete('/api/:id', function(req: Request, res: Response) {
+  service.delete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch((err: Error) => res.status(400));
+});
 
 router.use('/*', function(req: Request, res: Response) {
   res.sendStatus(404)
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,41 @@
 import Note from '../database/models';
 
 class Service {
+
   public async create(note: Partial<Note>): Promise<Note> {
     try {
       const data = await Note.create(note);
       return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  public async read(author: string): Promise<Note[]> {
+    try {
+      const data = await Note.findAll({ where: { author }, raw: true });
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  public async update(id: string, note: Partial<Note>): Promise<void> {
+    try {
+      await Note.update(note, { where: { id } });
+      return;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  public async delete(id: string): Promise<void> {
+    try {
+      await Note.destroy({ where: { id } });
+      return;
     } catch (error) {
       console.log(error);
       throw error;
